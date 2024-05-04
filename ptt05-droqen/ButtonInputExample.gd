4aeb1f51aa45bae804160e8b5bfb46b794bf4280
Alexander Martin <hi@droqen.com>
2024-05-04 00:42:41 -0400

    ptt05 - ...

--- ptt05-droqen/ButtonInputExample.gd
+++ ptt05-droqen/ButtonInputExample.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+var aheld : bool = false
 
 func _physics_process(_delta):
 	var pin = $Pin
 	var spr : Sprite = get_child(0)
 	spr.rotation = lerp(spr.rotation, PI*0.5 if pin.man.a else 0.0, 0.5)
 	spr.position = lerp(spr.position, pin.man.stick * 100, 0.5)
+	if aheld != pin.man.a: # if just pressed
+		aheld = pin.man.a
+		if aheld: $gumdrop.play()
